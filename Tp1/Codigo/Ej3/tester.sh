c9107e5483185983bb1fc43f69e9622dc7ba29bf
Franco <franconegri2004@gmail.com>
2014-08-26 15:56:51 -0300

    Modificado el generador para que no parezca que está colgado y para que no generee un millon de archivos en el mismo directorio

--- Tp1/Codigo/Ej3/tester.sh
+++ Tp1/Codigo/Ej3/tester.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 # declare STRING variable
+echo "Compilando Codigo..."
+g++ -std=c++0x Ej3.cpp -o testing/backtrack1
+g++ -std=c++0x Ej3_podas_invertidas.cpp -o testing/backtrack_invertido
+#g++ -std=c++0x Ej3_random.cpp -o testing/backtrack_random
+g++ -std=c++0x fuerzaBruta.cpp -o testing/fuerzaBruta
+echo "OK!"
 
-for VARIABLE in {10..13}
+back1="backtrack"
+back2="backtrack_invertido"
+back3="backtrack_random"
+back4="fuerzaBruta"
+
+NOMBRE="testing/tiempoConN"
+EXT=".txt"
+for VARIABLE in {1..15}
 do
-	NOMBRE="testing/salida"
-	EXT=".txt"
+	
 	python generador.py $VARIABLE
-	NOMBRE=$NOMBRE$VARIABLE$EXT
-	./a.out < testing/testej3.txt > $NOMBRE
+	echo "generada entrada para n:"$VARIABLE
+	echo "Corriendo Backtrack..."
+	./testing/backtrack1 < testing/testej3.txt > $NOMBRE$VARIABLE$back1$EXT
+	echo "Corriendo Backtrack Invertido..."
+	./testing/backtrack_invertido < testing/testej3.txt > $NOMBRE$VARIABLE$back2$EXT
+	echo "Corriendo Backtrack random..."
+	#./testing/backtrack_random < testing/testej3.txt > $NOMBRE$VARIABLE$back3$EXT
+	echo "Corriendo Fuerza Bruta..."
+	./testing/fuerzaBruta < testing/testej3.txt > $NOMBRE$VARIABLE$back4$EXT
+	echo "OK!"
 done
