be0531b08b3a0157054f0618c6a7d3511265d9d3
Ricardo Colombo <Ricardogcolombo@gmail.com>
2014-09-01 00:27:44 -0300

    Arreglos informe 2 y generador del 1 para casos de todos sanos

--- Tp1/Codigo/Ej1/generador.py
+++ Tp1/Codigo/Ej1/generador.py
@@ -18,12 +18,12 @@ fo.write("0")
 fo.close()
 fo = open("sanos.in", "wb")
 n = 100
-c = 10
+c = 1
 cantidad_de_tablones = 1000
 for x in range(1, cantidad_de_tablones):
     fo.write(str(x) + " " + str(randint(1, x)) + " ")
     for i in range(0, x):
-        tablon = randint(0, 1)
+        tablon = 0
         fo.write(str(tablon) + " ")
         fo.write("")
     fo.write("\n")
