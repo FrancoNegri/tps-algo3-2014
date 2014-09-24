a66b6cfb68e13237ec1f327615936657597f927e
Franco <franconegri2004@gmail.com>
2014-09-23 22:35:26 -0300

    Bugfix para el ej2, anda correctamente otra vez

--- tp2/src/ej_2/ej2_b.cpp
+++ tp2/src/ej_2/ej2_b.cpp
@@ -16,8 +16,8 @@
 
 
 //capaz habría que cambiar los nombes
-#define NODO_NO_MARCADO 6000
-#define NODO_MARCADO 7000
+#define NODO_NO_MARCADO INT_MAX
+#define NODO_MARCADO INT_MAX-1
 
 
 
@@ -52,7 +52,7 @@ tablero crear_tablero(int n)
 }
 
 
-void agregar_nodos_de_profunidad_k_mas_uno(coordenada nodo, queue<coordenada> *nodos_de_altura_k_mas_uno, tablero unTablero)
+void agregar_nodos_de_profunidad_k_mas_uno(coordenada nodo, queue<coordenada> *nodos_de_altura_k_mas_uno, tablero &unTablero)
 {
 	//chequeo el rango, si el caballo salta a una posicion valida y no salte previamente a esta posicion, lo agrego a la cola
 	//me aseguro de no meter dos veces en la cola el mismo nodo 'maracandolo', asi, si de dos nodos de altura k puedo saltar al mismo nodo de altura k+1, solo lo agrego la primera vez
@@ -173,7 +173,6 @@ int main()
 	// en este tablero voy guardando cuanto le cuesta al caballo i, llegar a la pocision (x,y) del tablero
 	vector<tablero> tablero_para_caballo_i(cantidad_de_caballos,crear_tablero(n));
 
-	int contadorDeCasillas = 0;
 	for(int caballo_i = 0; caballo_i < cantidad_de_caballos; caballo_i++)
 	{
 		 queue<coordenada> *nodos_de_altura_k = new queue<coordenada>;
@@ -197,12 +196,8 @@ int main()
 				 tablero_para_caballo_i[caballo_i].casillas[nodo.x][nodo.y] = k;
 
 				 agregar_nodos_de_profunidad_k_mas_uno(nodo, nodos_de_altura_k_mas_uno, tablero_para_caballo_i[caballo_i]);
-				 //contadorDeCasillas++;
-				 //cout << "casillas en esta profundidad" << contadorDeCasillas << endl;
 		 	}
 		 	//ok, ya no hay nodos de altura k, ahora paso a k+1, (que va a ser el nuevo k), y borro lo que había en k+1
-		 	//cout << "salto de k a k+1." << endl;
-		 	//cout << "k:" << k << endl;
 
 		 	delete nodos_de_altura_k;
 
@@ -212,12 +207,11 @@ int main()
 		 	
 		 	k++;
 		 }
-		 
 	}
 
 
 	//esto es para ver las matrices que quedan despues de hacer todos los bfeses.
-	
+	/*
 	for(int i = 0; i <  cantidad_de_caballos;i++)
 	{
 	cout << "Saltos necesarios para el caballo: " << i << " salte al casillero" << endl << endl;
@@ -227,17 +221,17 @@ int main()
 			{
 				if(tablero_para_caballo_i[i].casillas[j][k] > NODO_MARCADO)
 				{
-					cout << tablero_para_caballo_i[i].casillas[j][k] << " ";
+					cout << "no ";	
 				}
 				else
 				{
-					cout << "no ";	
+					cout << tablero_para_caballo_i[i].casillas[j][k] << " ";
 				}
 			}
 			cout << endl;
 		}
 		cout << endl;
-	}
+	}*/
 	
 	int suma_min = INT_MAX;
 	coordenada nodo_minimo;
@@ -281,8 +275,8 @@ int main()
 	/*if(suma_min == INT_MAX)
 		cout << "no" << endl;
 	else
-		cout << nodo_minimo.x + 1 << " " << nodo_minimo.y + 1 << " " << suma_min << endl;
-	return 0;*/
+		cout << nodo_minimo.x + 1 << " " << nodo_minimo.y + 1 << " " << suma_min << endl;*/
+	return 0;
 }
 
 
