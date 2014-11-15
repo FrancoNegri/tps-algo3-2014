fea957b3d22db1d41f746792bf673d2af8bc7650
Franco <franconegri2004@gmail.com>
2014-11-15 17:39:40 -0300

    Mas testing

--- tp3/Codigo/Testing/tester.sh
+++ tp3/Codigo/Testing/tester.sh
@@ -7,25 +7,22 @@ rm -rf $DIR
 mkdir $DIR
 
 echo "Compilando Codigo..."
-g++ generador_de_entrada.cpp -o $DIR/tester
-g++ Ej2/ej2.cpp -o $DIR/ej2
-g++ Ej3/ej3.cpp -o $DIR/ej3
-g++ Ej4/ej4.cpp -o $DIR/ej4
-g++ Ej5/ej5.cpp -o $DIR/ej5
+g++ -O2 generador_de_entrada.cpp -o $DIR/tester
+g++ -O2 Ej2/ej2.cpp -o $DIR/ej2
+g++ -O2 Ej3/ej3.cpp -o $DIR/ej3
+g++ -O2 Ej4/ej4.cpp -o $DIR/ej4
+g++ -O2 Ej5/ej5.cpp -o $DIR/ej5
 
 echo "OK!"
 
-for numeroDeNodos in {5..50}
+for numeroDeNodos in {3..40}
 do
-	echo "generada entrada para n:" $numeroDeNodos "con aristas de 1 a 100"
-	for numeroDeAristas in {100..100}
-	do
-        ./$DIR/tester $numeroDeNodos $(( (numeroDeNodos*(numeroDeNodos-1))/2 )) 10
-        ./$DIR/ej2 < $DIR/test.txt >> $DIR/resultados2.txt
-        ./$DIR/ej3 < $DIR/test.txt >> $DIR/resultados3.txt
-        ./$DIR/ej4 < $DIR/test.txt >> $DIR/resultados4.txt
-        ./$DIR/ej5 < $DIR/test.txt >> $DIR/resultados5.txt
-    done
+	echo "generada entrada para n:" $numeroDeNodos
+    ./$DIR/tester $numeroDeNodos $(( (numeroDeNodos*(numeroDeNodos-1))/2 )) 2
+    ./$DIR/ej2 < $DIR/test.txt >> $DIR/resultados2.txt
+    ./$DIR/ej3 < $DIR/test.txt >> $DIR/resultados3.txt
+    ./$DIR/ej4 < $DIR/test.txt >> $DIR/resultados4.txt
+    ./$DIR/ej5 < $DIR/test.txt >> $DIR/resultados5.txt
     echo "OK!"
 done
 
