507986206f9a4eaf2ac2bb7af3a5b995663ae3ea
Franco <franconegri2004@gmail.com>
2014-11-16 10:34:15 -0300

    salsla

--- tp3/Codigo/Ej4/busqueda_local2.cpp
+++ tp3/Codigo/Ej4/busqueda_local2.cpp
@@ -53,57 +53,54 @@ vector<int> busqueda_local(vector< vector< int> > &matriz_de_adyacencias, vector
 			}
 		}
 
-		for(int i = 0; i < n; i++)
+		//Ok, ahora se cuanto peso agrega tener a i en este conjunto en particular
+		//hay un conjunto donde i agregue menos peso?
+		// busco en mi vecindad, donde meter el nodo i para que sea minimo el peso
+		int conjunto_candidato; 
+		int suma_conjunto_candidato = INFINITO; 
+		for(int j = 0; j < k; j++)
 		{
-			//Ok, ahora se cuanto peso agrega tener a i en este conjunto en particular
-			//hay un conjunto donde i agregue menos peso?
-			// busco en mi vecindad, donde meter el nodo i para que sea minimo el peso
-			int conjunto_candidato; 
-			int suma_conjunto_candidato = INFINITO; 
-			for(int j = 0; j < k; j++)
+			//evito el subconjunto de i
+			if (j == conjuntoDelNodoQueMasPesoGenera)
+				continue;
+
+			//para cada conjunto hago la suma
+			int sumaPoniendoA_i_enOtroConjunto = 0;
+			
+			for(int w = 0; w < subconjuntos[j].size(); w++)
 			{
-				//evito el subconjunto de i
-				if (j == conjuntoDelNodoQueMasPesoGenera)
-					continue;
-
-				//para cada conjunto hago la suma
-				int sumaPoniendoA_i_enOtroConjunto = 0;
-				
-				for(int w = 0; w < subconjuntos[j].size(); w++)
-				{
-					sumaPoniendoA_i_enOtroConjunto += matriz_de_adyacencias[i][subconjuntos[j][w]];
-				}
-
-				//y me quedo con el minimo que es el candidato
-				if(sumaPoniendoA_i_enOtroConjunto < suma_conjunto_candidato)
-				{
-					suma_conjunto_candidato = sumaPoniendoA_i_enOtroConjunto;
-					conjunto_candidato = j;
-				}
+				sumaPoniendoA_i_enOtroConjunto += matriz_de_adyacencias[nodo][subconjuntos[j][w]];
 			}
 
-			//si mejore algo, muevo el nodo i al candidato
-			if(suma_conjunto_candidato < pesoNodoQueMasPesoGenera)
+			//y me quedo con el minimo que es el candidato
+			if(sumaPoniendoA_i_enOtroConjunto < suma_conjunto_candidato)
 			{
-				//cambio a i de conjunto
-				en_que_subconjunto_esta_cada_nodo[i] = conjunto_candidato;
-
-				//lo pongo en el conjunto nuevo...
-				subconjuntos[conjunto_candidato].push_back(i);
-
-				//lo saco del conjunto viejo...
-				for(int aux = 0; aux < subconjuntos[conjuntoDelNodoQueMasPesoGenera].size(); aux++)
-				{
-					if(subconjuntos[conjuntoDelNodoQueMasPesoGenera][aux] == i)
-					{
-						subconjuntos[conjuntoDelNodoQueMasPesoGenera].erase(subconjuntos[conjuntoDelNodoQueMasPesoGenera].begin() + aux);
-						break;
-					}							
-				}
-				//Como mejore, reinicio la busqueda local con el nodo i mudado
-				SeEncontroMejorSol = true;
-				break;
+				suma_conjunto_candidato = sumaPoniendoA_i_enOtroConjunto;
+				conjunto_candidato = j;
 			}
+		}
+
+		//si mejore algo, muevo el nodo i al candidato
+		if(suma_conjunto_candidato < pesoNodoQueMasPesoGenera)
+		{
+			//cambio a i de conjunto
+			en_que_subconjunto_esta_cada_nodo[nodo] = conjunto_candidato;
+
+			//lo pongo en el conjunto nuevo...
+			subconjuntos[conjunto_candidato].push_back(nodo);
+
+			//lo saco del conjunto viejo...
+			for(int aux = 0; aux < subconjuntos[conjuntoDelNodoQueMasPesoGenera].size(); aux++)
+			{
+				if(subconjuntos[conjuntoDelNodoQueMasPesoGenera][aux] == nodo)
+				{
+					subconjuntos[conjuntoDelNodoQueMasPesoGenera].erase(subconjuntos[conjuntoDelNodoQueMasPesoGenera].begin() + aux);
+					break;
+				}							
+			}
+			//Como mejore, reinicio la busqueda local con el nodo i mudado
+			SeEncontroMejorSol = true;
+			break;
 		}
 	}
 	return en_que_subconjunto_esta_cada_nodo;
