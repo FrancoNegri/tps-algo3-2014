9585d740518fd968fcad6cbbfcfabd7addc7b0e2
Franco <franconegri2004@gmail.com>
2014-12-17 13:30:23 -0300

    Otro Testing

--- tp3/Codigo/Testing2/testing/script.sh
+++
@@ -1,8 +0,0 @@
-for i in 2 3 4 7
-	do
-		sed -n 2~2p resultados$i.txt > tiempo$i
-		sed -n 1~2p resultados$i.txt > respuesta$i
-	done
-
-	paste tiempo2 tiempo3 tiempo4 tiempo7 > tiempos
-	paste respuesta2 respuesta3 respuesta4 respuesta7 > respuestas
