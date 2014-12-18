9e0de7f466b08ee099a36cad0e4e6714c8cbe96f
Franco <franconegri2004@gmail.com>
2014-12-18 17:23:49 -0300

    Algunas cositas

--- tp3/Codigo/Testing2/Ej4/busqueda_local.cpp
+++ tp3/Codigo/Testing2/Ej4/busqueda_local.cpp
@@ -26,70 +26,70 @@ using namespace std;
 
 vector<int> busqueda_local(vector< vector< int> > &matriz_de_adyacencias, vector< vector< int> > &subconjuntos, int k, int n, vector <int> en_que_subconjunto_esta_cada_nodo)
 {
-	//aca tengo que empezar a buscar localmente
-	bool SeEncontroMejorSol = true;
-	while(SeEncontroMejorSol)
-	{
-		SeEncontroMejorSol = false;
-		for(int i = 0; i < n; i++)
-		{
-			int conjuntoActualDe_i = en_que_subconjunto_esta_cada_nodo[i];
-			int sumaDelConjuntoDe_i = 0;
-			//Calculo cuanto suma el nodo i en el conjunto donde esta viviendo
-			for(int j = 0; j < subconjuntos[conjuntoActualDe_i].size(); j++) 
-			{
-				sumaDelConjuntoDe_i += matriz_de_adyacencias[i][subconjuntos[conjuntoActualDe_i][j]];
-			}
-			//Ok, ahora se cuanto peso agrega tener a i en este conjunto en particular
-			//hay un conjunto donde i agregue menos peso?
-			// busco en mi vecindad, donde meter el nodo i para que sea minimo el peso
-			int conjunto_candidato; 
-			int suma_conjunto_candidato = INFINITO; 
-			for(int j = 0; j < k; j++)
-			{
-				//evito el subconjunto de i
-				if (j == conjuntoActualDe_i)
-					continue;
+    //aca tengo que empezar a buscar localmente
+    bool SeEncontroMejorSol = true;
+    while (SeEncontroMejorSol)
+    {
+        SeEncontroMejorSol = false;
+        for (int i = 0; i < n; i++)
+        {
+            int conjuntoActualDe_i = en_que_subconjunto_esta_cada_nodo[i];
+            int sumaDelConjuntoDe_i = 0;
+            //Calculo cuanto suma el nodo i en el conjunto donde esta viviendo
+            for (int j = 0; j < subconjuntos[conjuntoActualDe_i].size(); j++)
+            {
+                sumaDelConjuntoDe_i += matriz_de_adyacencias[i][subconjuntos[conjuntoActualDe_i][j]];
+            }
+            //Ok, ahora se cuanto peso agrega tener a i en este conjunto en particular
+            //hay un conjunto donde i agregue menos peso?
+            // busco en mi vecindad, donde meter el nodo i para que sea minimo el peso
+            int conjunto_candidato;
+            int suma_conjunto_candidato = INFINITO;
+            for (int j = 0; j < k; j++)
+            {
+                //evito el subconjunto de i
+                if (j == conjuntoActualDe_i)
+                    continue;
 
-				//para cada conjunto hago la suma
-				int sumaPoniendoA_i_enOtroConjunto = 0;
-				
-				for(int w = 0; w < subconjuntos[j].size(); w++)
-				{
-					sumaPoniendoA_i_enOtroConjunto += matriz_de_adyacencias[i][subconjuntos[j][w]];
-				}
+                //para cada conjunto hago la suma
+                int sumaPoniendoA_i_enOtroConjunto = 0;
 
-				//y me quedo con el minimo que es el candidato
-				if(sumaPoniendoA_i_enOtroConjunto < suma_conjunto_candidato)
-				{
-					suma_conjunto_candidato = sumaPoniendoA_i_enOtroConjunto;
-					conjunto_candidato = j;
-				}
-			}
+                for (int w = 0; w < subconjuntos[j].size(); w++)
+                {
+                    sumaPoniendoA_i_enOtroConjunto += matriz_de_adyacencias[i][subconjuntos[j][w]];
+                }
 
-			//si mejore algo, muevo el nodo i al candidato
-			if(suma_conjunto_candidato < sumaDelConjuntoDe_i)
-			{
-				//cambio a i de conjunto
-				en_que_subconjunto_esta_cada_nodo[i] = conjunto_candidato;
+                //y me quedo con el minimo que es el candidato
+                if (sumaPoniendoA_i_enOtroConjunto < suma_conjunto_candidato)
+                {
+                    suma_conjunto_candidato = sumaPoniendoA_i_enOtroConjunto;
+                    conjunto_candidato = j;
+                }
+            }
 
-				//lo pongo en el conjunto nuevo...
-				subconjuntos[conjunto_candidato].push_back(i);
+            //si mejore algo, muevo el nodo i al candidato
+            if (suma_conjunto_candidato < sumaDelConjuntoDe_i)
+            {
+                //cambio a i de conjunto
+                en_que_subconjunto_esta_cada_nodo[i] = conjunto_candidato;
 
-				//lo saco del conjunto viejo...
-				for(int aux = 0; aux < subconjuntos[conjuntoActualDe_i].size(); aux++)
-				{
-					if(subconjuntos[conjuntoActualDe_i][aux] == i)
-					{
-						subconjuntos[conjuntoActualDe_i].erase(subconjuntos[conjuntoActualDe_i].begin() + aux);
-						break;
-					}							
-				}
-				//Como mejore, reinicio la busqueda local con el nodo i mudado
-				SeEncontroMejorSol = true;
-				break;
-			}
-		}
-	}
-	return en_que_subconjunto_esta_cada_nodo;
+                //lo pongo en el conjunto nuevo...
+                subconjuntos[conjunto_candidato].push_back(i);
+
+                //lo saco del conjunto viejo...
+                for (int aux = 0; aux < subconjuntos[conjuntoActualDe_i].size(); aux++)
+                {
+                    if (subconjuntos[conjuntoActualDe_i][aux] == i)
+                    {
+                        subconjuntos[conjuntoActualDe_i].erase(subconjuntos[conjuntoActualDe_i].begin() + aux);
+                        break;
+                    }
+                }
+                //Como mejore, reinicio la busqueda local con el nodo i mudado
+                SeEncontroMejorSol = true;
+                break;
+            }
+        }
+    }
+    return en_que_subconjunto_esta_cada_nodo;
 }
